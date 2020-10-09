2c36e7eeae66fe151293e94bdd5614a2ec209324
Marina Kriek <pauwmarina@gmail.com>
2020-10-09 16:39:30 +0200

    feat(AddDTOs)

---
+++ OLLIMS-API/Models/DTOs/InstrumentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OLLIMS_API.Models
+{ 
+    public partial class InstrumentDTO
+    {
+        public int Id { get; set; }
+        public int? LaboratoryId { get; set; }
+        public int? MeasurementId { get; set; }
+        public int? Sopid { get; set; }
+    }
+}
---
+++ OLLIMS-API/Models/DTOs/SOPDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OLLIMS_API.Models
+{
+    public partial class SOPDTO
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string InstructionUrl { get; set; }
+        public string DocumentUrl { get; set; }
+
+        public int InstrumentId { get; set; }
+
+        public virtual ICollection<SopTrainee> SopTrainees { get; set; }
+    }
+}
